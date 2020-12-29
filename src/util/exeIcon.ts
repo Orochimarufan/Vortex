8cdfe0e0e1cdf9cdbbd375b9aca9edfa780bd2a5
Taeyeon Mori <taeyeon@oro.sodimm.me>
2020-12-29 04:33:27 +0100

    WIP: optional icon-extract

--- src/util/exeIcon.ts
+++ src/util/exeIcon.ts
@@ -1,11 +1,18 @@
 import { app as appIn, remote } from 'electron';
-import * as iconExtract from 'icon-extract';
+//import * as iconExtract from 'icon-extract';
 import * as fs from './fs';
 
 const app = remote !== undefined ? remote.app : appIn;
 
+let iconExtract: any;
+if (process.platform === "win32") {
+  try {
+    iconExtract = require("icon-extract");
+  } catch (e) {};
+}
+
 function extractExeIcon(exePath: string, destPath: string): Promise<void> {
-  if (process.platform === 'win32') {
+  if (iconExtract !== undefined && process.platform === 'win32') {
     // app.getFileIcon generates broken output as of electron 11.0.4, afaik this
     // is limited to windows
     return new Promise((resolve, reject) => {
